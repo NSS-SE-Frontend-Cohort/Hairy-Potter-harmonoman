5c12ead003f98cfb6030a84cf88e672f5db196e1
Mark Bond <markbond615@gmail.com>
2025-01-29 08:27:06 -0500

    Added Kiln.js and firePottery function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 // Imports go first
-const  { makePottery } = require("./PotteryWheel.js");
+const { makePottery } = require("./PotteryWheel.js");
+const { firePottery } = require("./Kiln.js");
 
 
 // Make 5 pieces of pottery at the wheel
@@ -15,7 +16,16 @@ let vase = makePottery("vase", 6, 10)
 console.log(vase)
 
 // Fire each piece of pottery in the kiln
-
+let firedMug = firePottery(mug, 2200);
+console.log(firedMug)
+let firedCup = firePottery(cup, 2201);
+console.log(firedCup)
+let firedPlate = firePottery(plate, 2198);
+console.log(firedPlate)
+let firedBowl = firePottery(bowl, 2210);
+console.log(firedBowl)
+let firedVase = firePottery(vase, 2200);
+console.log(firedVase)
 
 // Determine which ones should be sold, and their price
 
